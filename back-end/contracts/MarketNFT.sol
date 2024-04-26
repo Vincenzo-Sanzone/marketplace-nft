45331bc7b388d1850776522a4c5db4d380588dee
Sanzone Vincenzo <vincenzo.sanzone02@gmail.com>
2024-04-26 18:17:08 +0200

    added mint function

--- back-end/contracts/MarketNFT.sol
+++ back-end/contracts/MarketNFT.sol
@@ -6,15 +6,15 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import {NFT} from "./NFT.sol";
 
 //memorize the address of the people who list a NFT with price
-struct NFTListing{
-    uint256 price;
-    address seller;
-}
+    struct NFTListing {
+        uint256 price;
+        address seller;
+    }
 
-contract MarketNFT is ERC721URIStorage, Ownable{
+contract MarketNFT is ERC721URIStorage, Ownable {
 
     //create the variable _owner
-   // address private _owner;
+    // address private _owner;
 
     //create a map with the token id mapped with NFTListing (aka memorize also the price and seller)
     mapping(uint256 => NFTListing) private _listings;
@@ -23,10 +23,15 @@ contract MarketNFT is ERC721URIStorage, Ownable{
         //we can suppose the one who deployed the contract is the owner
         //_owner = msg.sender;
     }
+
+    function mint(address to, string memory tokenURI) public {
+        new NFT().mint(to, tokenURI);
+    }
+
     //list NFT
-    function listNFT(uint256 tokenID, uint256 price) public{
+    function listNFT(uint256 tokenID, uint256 price) public {
         //check that the price is greater than 0
-        require(price> 0, "MarketNFT: price must be greater than 0");
+        require(price > 0, "MarketNFT: price must be greater than 0");
         //only the owner of the NFT can transfer the ownership
         //Gives permission to 'to' to transfer tokenId token to another account.
         //address(this) ??? -> https://ethereum.stackexchange.com/questions/40018/what-is-addressthis-in-solidity
@@ -34,19 +39,19 @@ contract MarketNFT is ERC721URIStorage, Ownable{
         //transfer th ownership of NFT to the contract(?)
         transferFrom(msg.sender, address(this), tokenID);
         // memorize the price and the address of who list the NFT
-        _listings[tokenID] = NFTListing(price,msg.sender);
+        _listings[tokenID] = NFTListing(price, msg.sender);
     }
 
     //buy NFT
-    function buyNFT(uint256 tokenID) public payable{
+    function buyNFT(uint256 tokenID) public payable {
         //check if the NFT with that tokenID is for sale
         NFTListing memory listing = _listings[tokenID];
-        require(listing.price>0, "MarketNFT: NFT not for sale");
+        require(listing.price > 0, "MarketNFT: NFT not for sale");
         //control if the price paid is the price of the NFT
         require(msg.value == listing.price, "MarketNFT: the price paid is not the value of the NFT");
         //transfer the ownership from the contract to the msg.sender
-        payable(listing.seller).transfer(listing.price*95/100); // !! we should use save-math !!
-        transferFrom(address(this), msg.sender,tokenID);
+        payable(listing.seller).transfer(listing.price * 95 / 100); // !! we should use save-math !!
+        transferFrom(address(this), msg.sender, tokenID);
         //we take 5% of the price
         // eliminate the NFT from listing
         clearListing(tokenID);
@@ -69,20 +74,19 @@ contract MarketNFT is ERC721URIStorage, Ownable{
         //if we implement manually the variable _owner
         //require(_owner == msg.sender, "MarketNFT: you are not the owenr!");
         //if we import Ownable.sol
-        uint256 balance =  address(this).balance;
+        uint256 balance = address(this).balance;
 
         require(balance > 0, "NFTMarket: balance is zero");
         payable(msg.sender).transfer(balance);
     }
 
-
     // eliminate the NFT with that tokenID from listing
     function clearListing(uint256 tokenID) private {
         _listings[tokenID].price = 0;
-        _listings[tokenID].seller= address(0);
+        _listings[tokenID].seller = address(0);
     }
 
-    function getListingByTokenID(uint256 tokenID)public  returns (uint256, address ){
-        return  (_listings[tokenID].price, _listings[tokenID].seller);
+    function getListingByTokenID(uint256 tokenID) public returns (uint256, address){
+        return (_listings[tokenID].price, _listings[tokenID].seller);
     }
 }
