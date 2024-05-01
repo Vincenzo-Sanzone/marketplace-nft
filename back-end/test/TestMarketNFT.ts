05f3ff3d3f77199cff7d7e2d3397b9b92a6ca3f9
Sanzone Vincenzo <vincenzo.sanzone02@gmail.com>
2024-05-01 17:00:29 +0200

    Added test for function cancel listing

--- back-end/test/TestMarketNFT.ts
+++ back-end/test/TestMarketNFT.ts
@@ -2,97 +2,98 @@ import {expect} from "chai";
 import hre from "hardhat";
 import {MarketNFT} from "../typechain-types";
 import {BigNumberish} from "ethers";
+import {HardhatEthersSigner} from "@nomicfoundation/hardhat-ethers/signers";
 
 const itParam = require('mocha-param').itParam;
 
 let market: MarketNFT
+const TOKEN_ID = 0;
+const PRICE = 10;
+const URL = "https://example.org";
+let owner : HardhatEthersSigner, other: HardhatEthersSigner;
+
 beforeEach(async function () {
-    market = await hre.ethers.deployContract("MarketNFT")
+    market = await hre.ethers.deployContract("MarketNFT");
+    [owner, other] = await hre.ethers.getSigners();
 })
 
 describe("Validation mint and list", () => {
     it("Should revert when price equal to 0", async () => {
-        const url = "https://example.org";
         const price = 0;
 
-        await expect(market.mintAndList(url, price)).to.be.revertedWith("Price must be greater than 0");
+        await expect(market.mintAndList(URL, price)).to.be.revertedWith("Price must be greater than 0");
     })
 
     it("Should list nft when everything is ok", async () => {
-        const url = "https://example.org";
-        const price = 10;
-
-        await expect(market.mintAndList(url, price)).to.not.be.reverted;
+        await expect(market.mintAndList(URL, PRICE)).to.not.be.reverted;
     })
 })
 
 describe("Validation list NFT", () => {
-
     it("Should revert when token id doesn't exist", async () => {
-        const tokenId = 0;
-        const price = 10;
-
-        await expect(market.listNFT(tokenId, price)).to.be.revertedWith("NFT is not present in the contract");
+        await expect(market.listNFT(TOKEN_ID, PRICE)).to.be.revertedWith("NFT is not present in the contract");
     })
 
     it("Should revert when address isn't the same of NFT's owner", async () => {
-        const tokenId = 0;
-        const price = 10;
-
-        const [owner, other] = await hre.ethers.getSigners();
-
         await market.mint(other.address, "https://example.org");
 
-        await expect(market.listNFT(tokenId, price)).to.be.revertedWith("You're not the owner of the NFT");
+        await expect(market.listNFT(TOKEN_ID, PRICE)).to.be.revertedWith("You're not the owner of the NFT");
     })
 })
 
 describe("Validation buy NFT", () => {
     it("Should revert when nft is not for sale", async () => {
-        const tokenId = 0
-
-        await expect(market.buyNFT(tokenId)).to.be.revertedWith("NFT is not for sale");
+        await expect(market.buyNFT(TOKEN_ID)).to.be.revertedWith("NFT is not for sale");
     })
 
 
     itParam("Should revert when price is not same than the NFT's price", [9, 11], async (offeredPrice: BigNumberish) => {
-        const url = "https://example.org";
-        const price = 10;
-
-        await market.mintAndList(url, price);
+        await market.mintAndList(URL, PRICE);
 
         await expect(market.buyNFT(0, {value: offeredPrice})).to.be.revertedWith("The price paid is not the value of the NFT");
     })
 
     it("Should revert when the sender is who listed nft", async () => {
-        const url = "https://example.org";
-        const price = 10;
+        await market.mintAndList(URL, PRICE);
 
-        await market.mintAndList(url, price);
-
-        await expect(market.buyNFT(0, {value: 10})).to.be.revertedWith("The owner can't buy his own NFT");
+        await expect(market.buyNFT(0, {value: PRICE})).to.be.revertedWith("The owner can't buy his own NFT");
     })
 
     it("Should update balance when the nft is sold", async () => {
-        const url = "https://example.org";
-        const price = 10;
+        await market.mintAndList(URL, PRICE);
 
-        const [owner, other] = await hre.ethers.getSigners();
-
-        await market.mintAndList(url, price);
-
-        await expect(market.connect(other).buyNFT(0, {value: 10})).to.changeEtherBalances([owner, other, market], [price - 1, -price, 1])
+        await expect(market.connect(other).buyNFT(TOKEN_ID, {value: PRICE})).to.changeEtherBalances([owner, other, market], [PRICE - 1, -PRICE, 1])
     })
 
     it("Should revert when another request is sent after buying ", async () => {
-        const url = "https://example.org";
-        const price = 10;
-
-        const [owner, other] = await hre.ethers.getSigners();
-
-        await market.mintAndList(url, price);
+        await market.mintAndList(URL, PRICE);
 
         await expect(market.connect(other).buyNFT(0, {value: 10})).to.not.reverted;
         await expect(market.buyNFT(0)).to.be.revertedWith("NFT is not for sale");
+    })
+})
+
+describe("Validation cancel listing", async () => {
+    it("Should revert when nft doesn't exist", async () => {
+        await expect(market.cancelListing(TOKEN_ID)).to.be.revertedWith("NFT is not for sale");
+    })
+
+    it("Should revert when the nft exists but it's not for sale", async () => {
+        await market.mint(owner.address, URL);
+
+        await expect(market.cancelListing(TOKEN_ID)).to.be.revertedWith("NFT is not for sale");
+    })
+
+    it("Should revert when the sender is not the owner of the NFT", async () => {
+        await market.mintAndList(URL, PRICE);
+
+        await expect(market.connect(other).cancelListing(TOKEN_ID)).to.be.revertedWith("You're not the seller of the NFT");
+    })
+
+    it("Should cancel listing when everything is ok", async () => {
+        await market.mintAndList(URL, PRICE);
+
+        await expect(market.cancelListing(TOKEN_ID)).to.not.be.reverted;
+        await expect(market.listNFT(TOKEN_ID, PRICE)).to.not.be.reverted;
     })
 })
